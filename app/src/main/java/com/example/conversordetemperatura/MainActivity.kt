f24b990b493a6d3894de2ce2d5b58ddaca6528e6
Juan <casal.juanmanuel@gmail.com>
2020-09-27 21:05:26 -0300

    Agregando toolbar

--- app/src/main/java/com/example/conversordetemperatura/MainActivity.kt
+++ app/src/main/java/com/example/conversordetemperatura/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.conversordetemperatura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Conversor de Temperatura"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.itemAyuda -> MostrarMensaje("En caso de ayuda, comunicase con ISTEA")
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun MostrarMensaje(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
     }
 }
